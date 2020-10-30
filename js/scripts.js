44326006280b99f0e33743b11bf06ba7fa5593a9
PatrikBeichner <patrik.beichner@gmail.com>
2020-10-30 19:22:02 -0400

    Carousels and scroll down indicator

--- js/scripts.js
+++ js/scripts.js
@@ -29,7 +29,48 @@ $(function () {
   $("#bookCoastal").click(function () {
     $("#coastalModal").modal("show");
   });
-  $(window).scroll(function(){
-    $("#fadeText").css("opacity", 1 - $(window).scrollTop() / ($('.parallax1').height() / 4));
+  $("#rentLink").click(function () {
+    $("#rentModal").modal("show");
+  });
+
+  $(".carousel").carousel({ interval: 4000, pause: "false" });
+  $("#carouselButton").click(function () {
+    if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+      $(".carousel").carousel("pause");
+      $("#carouselButton").children("i").removeClass("fa-pause");
+      $("#carouselButton").children("i").addClass("fa-play");
+    } else {
+      $(".carousel").carousel("cycle");
+      $("#carouselButton").children("i").removeClass("fa-play");
+      $("#carouselButton").children("i").addClass("fa-pause");
+    }
+  });
+
+  /*$(window).scroll(function(){
+    $("#fadeText").css("opacity", 1 - $(window).scrollTop() / 250);
+  });*/
+  /*$(window).scroll(function(){
+    $("#fadeText").css("opacity", 1 - $(window).scrollTop() / ($('.parallax1').height() / 10));
+});*/
+var documentEl = $(document),
+  fadeElem = $('#fadeText');
+
+  documentEl.on('scroll', function() {
+    var currScrollPos = documentEl.scrollTop();
+
+    fadeElem.each(function() {
+      var $this = $(this),
+        elemOffsetTop = $this.offset().top;
+      if (currScrollPos > elemOffsetTop) $this.css('opacity', -1 -(currScrollPos-elemOffsetTop)/400);
+    })
+  })
 });
-});
+
+function toggler(rural) {
+  $("#" + rural).toggle();
+}
+
+function toggler(urban) {
+  $("#" + urban).toggle();
+}
+
